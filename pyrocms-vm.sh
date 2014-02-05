289a37d56f461dc75bac8debcdd4bc404247fc8e
Lorenzo Garcia <lorenzogarcia.developer@gmail.com>
2014-02-05 09:11:25 +0000

    Remove pyrocms-vm.sh

--- pyrocms-vm.sh
+++
@@ -1,9 +0,0 @@
-git remote add -f pyrocms-vm git@github.com:LorenzoGarcia/pyrocms-vm.git
-
-git subtree add --prefix vm pyrocms-vm master --squash
-
-git fetch pyrocms-vm master
-
-git subtree pull --prefix vm pyrocms-vm master --squash
-
-git subtree push --prefix=vm pyrocms-vm master
